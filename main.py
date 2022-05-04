3b7fe73aa1bb8eaa8d83ef7e8d530b1969b2718c
repinpk <repin_petr@list.ru>
2022-05-04 23:24:41 +1000

    New version

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ goods_list = data.split('\n')
 
 def get_goods_info(goods_list):
     result = {}
-    expensive_goods = set()
     min_price = float('inf')
     min_count = float('inf')
     max_price = 0
@@ -17,56 +16,77 @@ def get_goods_info(goods_list):
     b = []
     c = []
     d = []
+    e = []
+    o = []
+    f = []
+    j = []
+
     for good in goods_list:
         item = good.split(':')
         item_price = int(item[1])
         item_count = int(item[2])
-        item_zorro = item[0]
         price_sum += item_price
-        # print(item[1])
         if item_price <= min_price:
             min_price = item_price
-            a.append(item_zorro),
             a.append(item_price)
-
         elif item_price >= max_price:
             max_price = item_price
-            b.append(item_zorro),
             b.append(item_price)
-        if item_count < min_count:
+        if item_count <= min_count:
             min_count = item_count
-            c.append(item_zorro)
-            c.append(item_count)
+            e.append(item_count)
         elif item_count > max_count:
             max_count = item_count
-            d.append(item_zorro)
-            d.append(item_count)
+            o.append(item_count)
         goods_count += 1
-    a1 = a[4:None]
-    b1 = b[10:None]
-    c1 = c[-2:None]
-    d1 = d[-2:None]
-
     mean_price = round(price_sum / goods_count, 2)
+    bor = min(a)
+    bar = max(b)
+    ber = min(e)
+    bir = max(o)
 
-    result['min_price'] = a1
-    result['max_price'] = b1
+
+    for k in goods_list:
+        item1 = k.split(':')
+        item_zorro1 = item1[0]
+        item_price1 = int(item1[1])
+        item_count1 = int(item1[2])
+        if item_price1 <= bor:
+            bor = item_price1
+            c.append(item_zorro1),
+            c.append(item_price1)
+        elif item_price1 >= bar:
+            bar = item_price1
+            d.append(item_zorro1),
+            d.append(item_price1)
+        if item_count1 <= ber:
+            ber = item_count1
+            f.append(item_zorro1),
+            f.append(item_count1)
+        elif item_count1 >= bir:
+            bir = item_count1
+            j.append(item_zorro1),
+            j.append(item_count1)
+
+
+
+    result['min_price'] = c
+    result['max_price'] = d
+    result['min_count'] = f
+    result['max_count'] = j
     result['mean_price'] = mean_price
-    result['min_count'] = min_count
-    result['min_count1'] = c1
-    result['max_count'] = max_count
-    result['max_count1'] = d1
+
     return result
 
 def print_goods_info(goods_list):
 
     goods_dict = get_goods_info(goods_list)
 
-    print(f'Самый дорогой товар: {goods_dict["max_price"]}',
-          f'Самый дешевый товар: {goods_dict["min_price"]}',
+    print(f'Самый дешевый товар: {goods_dict["min_price"]}',
+          f'Самый дорогой товар: {goods_dict["max_price"]}',
+          f'Товар с минимальным количеством: {goods_dict["min_count"]}',
+          f'Товар с максимальным количеством: {goods_dict["max_count"]}',
           f'Средняя цена товара: {goods_dict["mean_price"]}',
-          f'Минимальное количество товара: {goods_dict["min_count"]}, товар: {goods_dict["min_count1"]}',
-          f'Максимальное количество товара: {goods_dict["max_count"]}, товар: {goods_dict["max_count1"]}',
           sep='\n'
           )
 
